a093f4738bbdb587adcba7cc88e2107a4a0fdc94
studysewa <56931576+studysewa@users.noreply.github.com>
2019-12-23 05:11:08 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div>
+	<form action="userinfo.php" method="post">
+		<label>Username</label>
+		<input type="text" name="user"><br>
+		<label>email</label>
+		<input type="text" name="email"><br>
+		<label>mobile</label>
+		<input type="text" name="mobile"><br>
+		<button type="submit">Submit</button>
+	</form>
+</div>
+</body>
+
+</html>
